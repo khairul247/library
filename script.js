abbdcdb991f9b643db1368f3318c5ea9e0caf65e
Muhammad Khairul Ikhwan <khairulikhwan@Muhammads-MacBook-Air.local>
2024-07-06 00:34:43 +0800

    Add functional toggle read status button, the status'color change accordingly.

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const myLibrary = [];
 const form = document.querySelector(".my-form");
 const showBtn = document.getElementById("show-dialog");
 const container = document.querySelector(".container");
-const deleteBtn = document.querySelector(".delete-btn")
-const addBook = document.querySelector(".add-book")
+const deleteBtn = document.querySelector(".delete-btn");
+const addBook = document.querySelector(".add-book");
 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
@@ -38,6 +38,17 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+Book.prototype.toggleReadStatus = function () {
+    if (this.read === "Finished") {
+        this.read = "In Progress";
+    } else if (this.read === "In Progress") { 
+        this.read = "Not Yet";
+    } else {
+        this.read = "Finished";
+    }
+};
+
+
 
 function addBookToLibrary(myLibrary) {
     let book = myLibrary[myLibrary.length-1];
@@ -53,12 +64,23 @@ function createCard(book) {
     att.value = "delete-btn";
     
     for (let key in book){
-    const list = document.createElement('li');
-    list.classList.add(key);
-    list.textContent = book[key];
-    card.appendChild(list)
+
+        if (typeof book[key] === 'function') continue;
+        const list = document.createElement('li');
+        list.classList.add(key);
+        list.textContent = book[key];
+        card.appendChild(list)
     }
 
+    const readElement = card.querySelector(".read");
+    updateReadColor(readElement,book.read)
+
+    createDeleteBtn(card,att)
+    createToggleBtn (card,book,readElement)
+
+}
+
+function createDeleteBtn (card,att) {
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Delete";
     deleteBtn.setAttributeNode(att);
@@ -66,5 +88,27 @@ function createCard(book) {
     deleteBtn.addEventListener("click",(e) => {
         e.target.parentElement.remove();
     })
+}
 
+function createToggleBtn (card,book,readElement) {
+const toggleBtn = document.createElement("button");
+toggleBtn.textContent = "Toggle Read Status";
+card.appendChild(toggleBtn);
+
+toggleBtn.addEventListener("click", () => {
+    book.toggleReadStatus();
+    readElement.textContent = book.read;
+    updateReadColor(readElement,book.read)
+})
+
+}
+
+function updateReadColor (element,status) {
+    if (status === "Finished") {
+        element.style.color = "green";
+    } else if (status === "In Progress") {
+        element.style.color = "orange";
+    } else {
+        element.style.color = "red";
+    }
 }
